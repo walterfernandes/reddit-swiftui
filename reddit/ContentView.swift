dd9db182fdc3eb20cefcff790aa8c223b456dbaf
Walter Fernandes de Carvalho <walter.carvalho.trd@c6bank.com>
2020-08-09 17:21:25 -0300

    Initial Commit

---
+++ reddit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  reddit
+//
+//  Created by Walter Fernandes de Carvalho on 09/08/20.
+//  Copyright © 2020 walterfernandes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
